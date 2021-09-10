d0c2e5b982e8516d5fb8b80552774ae6b5214818
Jeremy Soller <jeremy@system76.com>
2021-09-09 20:03:55 -0600

    Improve check.py output

--- check.py
+++ check.py
@@ -16,7 +16,7 @@ def pop_origins(ver):
 if len(sys.argv) >= 2:
     SUITE = sys.argv[1]
 
-outdated = 0
+outdated = {}
 with tempfile.TemporaryDirectory() as rootdir:
     source_dir = f"{rootdir}/etc/apt/sources.list.d"
     os.makedirs(source_dir)
@@ -35,12 +35,12 @@ with tempfile.TemporaryDirectory() as rootdir:
     pop_release = repolib.DebLine("deb http://apt.pop-os.org/release " + SUITE + " main")
     add_source("pop-os-release", pop_release)
 
-    print("\x1B[1mUPDATING CACHE\x1B[0m")
+    print("\x1B[1mupdating cache\x1B[0m")
     cache = apt.Cache(rootdir=rootdir, memonly=True)
     cache.update()
     cache.open()
 
-    print("\x1B[1mOUT OF DATE:\x1B[0m")
+    print("\x1B[1mchecking binary packages\x1B[0m")
     for pkg in cache:
         max_ver = max(pkg.versions)
         pop_ver = max((v for v in pkg.versions if any(pop_origins(v))), default=None)
@@ -50,8 +50,11 @@ with tempfile.TemporaryDirectory() as rootdir:
             print(pkg)
             print(f"   {pop_origin.origin}:\t{pop_ver.source_name}\t{pop_ver.source_version}")
             print(f"   {max_origin.origin}:\t{max_ver.source_name}\t{max_ver.source_version}")
-            outdated += 1
+            if pop_ver.source_name == max_ver.source_name:
+                outdated[pop_ver.source_name] = True
 
-print(f"\x1B[1m{outdated} packages are out of date\x1B[0m")
-if outdated > 0:
+print(f"\x1B[1m{len(outdated)} source package(s) out of date\x1B[0m")
+for source_name in outdated:
+    print(f"  - {source_name}")
+if len(outdated) > 0:
     sys.exit(1)
