40877ba6f0f301c884332b3886e6fb9afd6aca9e
Jeremy Soller <jackpot51@gmail.com>
2025-02-27 10:50:16 -0700

    Limit flatpak appstream generator to one CPU

--- build.sh
+++ build.sh
@@ -469,8 +469,9 @@ function repo_build {
         done
 
         pushd ../..
+        # Run appstream-generator on only one CPU to prevent crashes
         set -x
-        flatpak run org.freedesktop.appstream.generator --config "${ASGENFILE}" run "${dist}"
+        taskset --cpu-list 0 flatpak run org.freedesktop.appstream.generator --config "${ASGENFILE}" run "${dist}"
         set +x
         popd
         for comp in "${COMPONENTS[@]}"
